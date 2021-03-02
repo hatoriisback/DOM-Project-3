a462d256168adf965abefae4938ad183ef9047b4
hendhendy <hendy.maxv@gmail.com>
2021-03-02 16:50:53 +0700

    MANTAP

---
+++ script.js
@@ -0,0 +1,14 @@
+// // Pig Games | Dice // //
+
+'use strict';
+
+const names = document.querySelectorAll('.name');
+const scores = document.querySelectorAll('.score');
+const currentScore = document.querySelectorAll('.current-score');
+
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
